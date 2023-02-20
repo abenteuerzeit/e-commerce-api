5533ac56242633a2051da091b697a60223ada5ba
abenteuerzeit <amroz.muzyka@gmail.com>
2023-02-20 01:57:01 +0100

    update server and create react redux views

--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import session from "express-session";
+import cors from "cors";
 import morgan from "morgan";
 import routes from "./routes";
-import cors from "cors";
+import helmet from "helmet";
+import nunjucks  from "../nunjucks";
+
 import cookieParser from "cookie-parser";
 // import bodyParser from "body-parser";
-import session from "express-session";
+
 import restartDatabase from "./model/querries/_util";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+const store = new session.MemoryStore();
 const resetDb = process.env.RESET_DB.toLowerCase() === "true";
+
+app.use(helmet());
+
+
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -35,13 +44,63 @@ app.use(
 );
 app.use(express.json());
 
+app.use(
+  session({
+    secret: process.env.COOKIES_SESSION_SECRET,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      secure: false,
+    },
+    saveUninitialized: false,
+    resave: false,
+    store,
+    })
+);
+
+function ensureAuthentication(req, res, next) {
+  console.log('Authenticated: ', req.session.authenticated);
+  if (req.session.authenticated) {
+    return next();
+  } else {
+    return res.status(403).json({ message: "Not authenticated" });
+  }
+}
+
+app.get('/api', ensureAuthentication, (req, res) => {
+  return res.json({ message: 'Hello from the API' });
+});
+
+
+app.get('/api/login', (req, res) => {
+  return res.json({ message: 'Hello from the login' });
+});
+
+app.post('/api/logout', (req, res) => {
+  if (!req.session.authenticated) {
+    return res.status(403).json({ message: 'Not authenticated' });
+  }
+  req.session.authenticated = false;
+  return res.json({ message: 'Logged out' });
+});
+
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'admin' && password === 'admin') {
+    req.session.authenticated = true;
+    return res.json({ message: 'Authenticated' });
+  } else {
+    return res.status(403).json({ message: 'Bad credentials' });
+  }
+});
+
+
+
 app.get("/", (req, res, next) => {
   if (req.query.name) {
     return res.status(200).json({ message: `Hello ${req.query.name}` });
   }
-  return res
-    .status(200)
-    .json({ message: "E-commerce API: Language Learning GET route OK" });
+  return res.status(200).json({ message: "Hello World" });
 });
 
 routes(app);
--- test/test.js
+++ test/test.js
@@ -1,3 +1,75 @@
 import request from 'supertest';
 import app from '../src/index';
 
+describe('GET /api', () => {
+    describe('when the user is not authenticated', () => {
+        it('should return 403', (done) => {
+            request(app)
+                .get('/api')
+                .expect(403, done);
+        });
+    });
+    describe('when the user is authenticated', () => {
+        it('should return 200', (done) => {
+            request(app)
+                .post('/api/login')
+                .send({ username: 'admin', password: 'admin' })
+                .end((err, res) => {
+                    request(app)
+                        .get('/api')
+                        .set('Cookie', res.headers['set-cookie'])
+                        .expect(200, done);
+                });
+        });
+    });
+});
+
+describe('POST /api/login', () => {
+    describe('when the user is not authenticated', () => {
+        it('should return 403', (done) => {
+            request(app)
+                .post('/api/login')
+                .send({ username: 'admin', password: 'wrong' })
+                .expect(403, done);
+        });
+    });
+    describe('when the user is authenticated', () => {
+        it('should return 200', (done) => {
+            request(app)
+                .post('/api/login')
+                .send({ username: 'admin', password: 'admin' })
+                .expect(200, done);
+        });
+    });
+});
+
+describe('POST /api/logout', () => {
+    describe('when the user is not authenticated', () => {
+        it('should return 403', (done) => {
+            request(app)
+                .post('/api/logout')
+                .expect(403, done);
+        });
+    });
+    describe('when the user is authenticated', () => {
+        it('should return 200', (done) => {
+            request(app)
+                .post('/api/login')
+                .send({ username: 'admin', password: 'admin' })
+                .end((err, res) => {
+                    request(app)
+                        .post('/api/logout')
+                        .set('Cookie', res.headers['set-cookie'])
+                        .expect(200, done);
+                });
+        });
+    });
+});
+describe('GET /', () => {
+    it('should return 200', (done) => {
+        request(app)
+            .get('/')
+            .expect(200, done);
+    });
+});
+
